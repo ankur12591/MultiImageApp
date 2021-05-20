20ffbacc46f23ea28267555915a71a679bb71cfd
Ankur <ankur12591@gmail.com>
2021-05-20 17:32:08 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/multi_image_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.multi_image_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // List<Asset> images = List<Asset>();
+  List<Asset> images = <Asset>[];
+
+  String _error = 'No Error Dectected';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> loadAssets() async {
+    List<Asset> resultList = <Asset>[];
+    String error = 'No Error Detected';
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 300,
+        enableCamera: true,
+        selectedAssets: images,
+        cupertinoOptions: CupertinoOptions(takePhotoIcon: "chat"),
+        materialOptions: MaterialOptions(
+          actionBarColor: "#abcdef",
+          actionBarTitle: "Example App",
+          allViewTitle: "All Photos",
+          useDetailsView: false,
+          selectCircleStrokeColor: "#000000",
+        ),
+      );
+    } on Exception catch (e) {
+      error = e.toString();
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      images = resultList;
+      //_error = error;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        // height: 700,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.only(top: 50),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //Center(child: Text('Error: $_error')),
+            ElevatedButton(
+              child: Text("Pick images"),
+              onPressed: loadAssets,
+            ),
+            Expanded(
+              child: Container(
+                  padding: EdgeInsets.all(20), child: buildGridView()),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildGridView() {
+    return Container(
+      child: GridView.count(
+        crossAxisCount: 3,
+        mainAxisSpacing: 5.0,
+        crossAxisSpacing: 5.0,
+        children: List.generate(images.length, (index) {
+          Asset asset = images[index];
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+
+            //color: Colors.orange,
+            child: AssetThumb(
+              asset: asset,
+              width: 300,
+              height: 300,
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
